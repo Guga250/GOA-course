ea8f2971134555827653601c97d72204c582226d
Guga250 <gugacho30@gmail.com>
2025-01-01 13:28:00 +0400

    Add files via upload

---
+++ day 12/classwork/cw1.py
@@ -0,0 +1,21 @@
+a1 = 10>4 #true
+a2 = 5>0 #true
+a3 = 7>99 #false
+a4 = 10000 < 2 #false
+a5 = 30 > 3 #true
+a6 = 55 >55555 #false
+a7 = 10101010101 < 1 #false
+a8 = 1 > 2 #false
+a9 = 2 > 1 #true
+a10 = 4349530475837783450 >22384729347234987234223 #false
+
+print(a1) 
+print(a2)
+print(a3)
+print(a4)
+print(a5)
+print(a6)
+print(a7)
+print(a8)
+print(a9)
+print(a10)
