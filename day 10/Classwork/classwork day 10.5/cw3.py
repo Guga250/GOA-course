9de55738e904e1701e6e5f1175a3cb357f7f991c
Guga250 <gugacho30@gmail.com>
2025-01-01 13:23:02 +0400

    Delete day 10 directory

--- day 10/Classwork/classwork day 10.5/cw3.py
+++
@@ -1,6 +0,0 @@
-a1 = int(input("enter number: "))
-b2 = int(input("enter number: "))
-print(a1 + b2)
-print(a1 - b2)
-print(a1 * b2)
-print(a1 / b2)
--- day 10/Homework/hw2.py
+++
@@ -1,7 +0,0 @@
-#explisity არის მონაცემის ჩვენით შეცვლა მაგ:
-number = 5.0
-num = int(number)
-print(num)
-#implisity არის თვითონ რომ იცვლის მონაცემს მაგ:
-variable = 200/5
-print(variable)
