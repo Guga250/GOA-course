d944636d19ed86f983d5bf83d492cacdb13eb075
Guga250 <gugacho30@gmail.com>
2025-03-26 01:21:35 -0700

    Add files via upload

---
+++ day 014/classwork/cw2.py
@@ -0,0 +1,10 @@
+#and
+a = True and True #True
+b = False and True #False
+c = False and False # False
+
+#or
+
+d =True or True # True 
+e = True or False # True
+f = False or False #Fals
---
+++ day 014/homework/hw3.py
@@ -0,0 +1,12 @@
+one_input = input("enter your name: ")
+two_input = int(input("enter your age: "))
+three_input = float(input("enter your height: "))
+
+while one_input != "guga":
+    one_input = input("enter your name: ")
+
+while two_input < 18:
+    two_input = int(input("enter your age: "))
+
+while three_input < 1.80:
+    three_input = float(input("enter your height: "))
