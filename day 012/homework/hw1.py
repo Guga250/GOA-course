b6f4fbce862adcdb3425e0e7b303c7e40e171f14
Guga250 <gugacho30@gmail.com>
2025-03-26 01:20:33 -0700

    Add files via upload

---
+++ day 012/homework/hw1.py
@@ -0,0 +1,33 @@
+a = 5 == 5 #true because they are same number
+b = 10 != 20 #true because they aren't same
+c = 4 > 3 and 2 < 5 #true because they are both real
+d = 10 > 20 or 5 < 8 #true because we must have 1 or more right
+e = not (7 > 3) #false because this is true but it says not front of 7 > 3 so it meanw terminal has to write opposite of true
+f = "hello" == "world" #false because theese words aren't same
+g = 15 >= 10 #true because 15 is more than 10
+h = 8 <= 8 #true because they are the same number.
+i = 3 > 1 and 2 == 2 and 4 != 5 #tue because these are right
+j = not (10 < 5 or 2 == 2) #false becayse these are true but not code is here so terminal has to write opposite of true
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+
+print("next")
+
+gghh = f and g
+jjaa = j or a
+ddaa=d or a
+jjee=not j or e
+
+print(gghh)
+print(jjaa)
+print(ddaa)
+print(jjee)
