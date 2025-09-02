7db71dda9de63659c4356aacfa910fef9a9d5af6
Guga250 <gugacho30@gmail.com>
2025-09-02 02:33:12 -0700

    Add files via upload

---
+++ day 042/classwork/cw3.py
@@ -0,0 +1,6 @@
+def check_for_factor(base, factor):
+    if base%factor ==0:
+        return True
+    else:
+        return False
+#https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/python
