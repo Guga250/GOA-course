c8324ad2e8d3c7df7d117048f155cbe4d7dbc231
Guga250 <gugacho30@gmail.com>
2026-04-11 01:41:39 -0700

    Add files via upload

---
+++ day 060/classwork/cw1.js
@@ -0,0 +1,39 @@
+//1
+let numm = -10
+console.log(Math.abs(numm))
+//2
+let numm2 = 10.1111112
+console.log(Math.floor(numm2))
+//3
+let sm = -4
+let sm1 = -5
+let sm2 = 10
+console.log(Math.min(sm, sm1, sm2))
+//4
+let bg = 10
+let bg2 = -100
+let bg3 = 1
+console.log(Math.max(bg, bg2, bg3))
+//5
+let nummer = 11
+let square = 2
+console.log(Math.pow(nummer, square))
+//6
+let nah = 20
+console.log((Math.PI)*nah)
+//7
+let nah2 = 25
+console.log((Math.E)*nah)
+//8
+let guga = 10
+let jaja = 20
+let mi = Math.min(guga, jaja)
+console.log(Math.pow(mi))
+//9
+let ha = 10.111
+let uha = 10.999
+let minion = Math.max(Math.floor(ha), Math.floor(uha))
+console.log(minion)
+//10
+let inten = -99
+console.log(Math.floor(Math.abs(inten)))
---
+++ day 060/homework/hw.js
@@ -0,0 +1,21 @@
+//1
+let am = Math.max(10, 4, -2)
+console.log(am*Math.PI)
+//2
+let con = Math.abs(-2)
+console.log(Math.pow(con, 2))
+//3
+let arr1 = 3.1
+let arr2 = 91.2
+let arr3 = 4.9999
+console.log(Math.min(arr1, arr2, arr3))
+//4
+let ar1 = 3
+let ar2 = 20
+let er1_1 =  Math.pow(ar1, 3)
+let er2_1 = Math.pow(ar2, 3)
+console.log(Math.max(er1_1, er2_1))
+//5
+let ah = -1.1
+let king = Math.floor(ah)
+console.log(Math.abs(king))
