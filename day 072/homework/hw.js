0ec054e7c8cec8dbfab325d705463966ddd2e4bf
Guga250 <gugacho30@gmail.com>
2026-07-25 03:26:17 -0700

    Add files via upload

---
+++ day 072/homework/hw.js
@@ -0,0 +1,30 @@
+//11
+for(let i = 1; i<100; i++){
+    if(i%7===0){
+        console.log(i)
+    }
+}
+//12
+for(let i = 30; i>1; i--){
+    if(i%2===0 || i%3===0){
+        console.log(i)
+    }
+}
+//13
+for(let i = 1; i<50; i++){
+    if(i%5!==0){
+        console.log(i)
+    }
+}
+//14
+for(let i = 80; i>40; i--){
+    if(i>60){
+        console.log(i)
+    }
+}
+//15
+for(let i =1; i<100; i++){
+    let b = 0
+    b+=i
+    console.log(b)
+}
