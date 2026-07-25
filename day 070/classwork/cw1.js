532dfe0ef527cb7a2a27edf0016a12ac20f525dd
Guga250 <gugacho30@gmail.com>
2026-07-25 03:25:54 -0700

    Add files via upload

---
+++ day 070/classwork/cw1.js
@@ -0,0 +1,14 @@
+
+let arr = [["Hello", "mid", "bye"], [5, -5, 25], [true, false, false]]
+if (arr[0][0]== "aleksandre"){
+    console.log("succes")
+}
+else if (arr[1][2]==10){
+    console.log("succes")
+}
+else if (arr[2][0]== true){
+    console.log("succes")
+    
+}else{
+    console.log("failure")
+}
